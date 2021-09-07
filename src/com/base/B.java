23ce95c3f14ba08505b8deb3f81cab5d94c99efe
lebron <1227781351@qq.com>
2021-09-07 21:03:37 +0800

    修改了B.class

--- src/com/base/B.java
+++ src/com/base/B.java
@@ -3,6 +3,7 @@ package com.base;
 public class B extends TestA {
 	public int a = 1;
 	 public void fun(){
+	 		System.out.println("dsafsdafasdf");
 	        System.out.println("-----B-----");
 	    }
 }
